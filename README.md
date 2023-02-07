1a04666ece9f99bbfa25a45013c2145113fcaf66
Sourav2707 <smartsourav33@gmail.com>
2023-02-07 16:51:44 +0530

    Initial commit with welcome message

---
+++ README.md
@@ -0,0 +1,1 @@
+LFP batch 235 - day 30 cab invoice generator program
---
+++ src/main/java/com/brigelabs_cab_invoice_generator/Main.java
@@ -0,0 +1,7 @@
+package com.brigelabs_cab_invoice_generator;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to day 30 cab invoice generator program");
+    }
+}
