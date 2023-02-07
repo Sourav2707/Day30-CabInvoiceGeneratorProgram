f9dee7b8573d47bc3abd423b53ba70c960e502e6
Sourav2707 <smartsourav33@gmail.com>
2023-02-07 17:49:20 +0530

    UC2 The invoice generator should now take in multiple rides and calculate the aggregate total for all

--- src/main/java/com/brigelabs_cab_invoice_generator/InvoiceGenerator.java
+++ src/main/java/com/brigelabs_cab_invoice_generator/InvoiceGenerator.java
@@ -8,17 +8,8 @@ public class InvoiceGenerator {
     final double farePerKm = 10;
     final double minFare = 5;
     double totalFare = 0;
-    public double calculateFare(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter number of rides");
-        int rides = scanner.nextInt();
-        for(int i = 0; i < rides; i++) {
-            System.out.println("Enter the distance travelled");
-            double distance = scanner.nextDouble();
-            System.out.println("Enter the time travelled");
-            double timeInMin = scanner.nextDouble();
-            totalFare = totalFare + (distance * farePerKm) + (timeInMin) + minFare;
-        }
+    public double calculateFare(double distance, double timeInMin){
+        totalFare = totalFare + (distance * farePerKm) + (timeInMin) + minFare;
         return totalFare;
     }
 }
--- src/main/java/com/brigelabs_cab_invoice_generator/Main.java
+++ src/main/java/com/brigelabs_cab_invoice_generator/Main.java
@@ -9,6 +9,16 @@ public class Main {
 
         System.out.println("Welcome to day 30 cab invoice generator program");
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-        System.out.println(invoiceGenerator.calculateFare());
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number of rides");
+        int rides = scanner.nextInt();
+        for(int i = 0; i < rides; i++) {
+            System.out.println("Enter the distance travelled");
+            double distance = scanner.nextDouble();
+            System.out.println("Enter the time travelled");
+            double timeInMin = scanner.nextDouble();
+            totalFare = invoiceGenerator.calculateFare(distance, timeInMin);
+        }
+        System.out.println(totalFare);
     }
 }
