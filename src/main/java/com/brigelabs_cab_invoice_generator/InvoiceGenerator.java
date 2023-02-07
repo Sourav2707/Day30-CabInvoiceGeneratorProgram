335cdda1fcc14f0140fe4a8cd298f6a67cfe3b4d
Sourav2707 <smartsourav33@gmail.com>
2023-02-07 17:32:05 +0530

    UC2 The invoice generator should now take in multiple rides and calculate the aggregate total for all

--- src/main/java/com/brigelabs_cab_invoice_generator/InvoiceGenerator.java
+++ src/main/java/com/brigelabs_cab_invoice_generator/InvoiceGenerator.java
@@ -1,10 +1,24 @@
 package com.brigelabs_cab_invoice_generator;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class InvoiceGenerator {
     final double farePerKm = 10;
     final double minFare = 5;
-    public double calculateFare(double distance, double timeInMin){
-        double totalFare = (distance * farePerKm) + (timeInMin) + minFare;
+    double totalFare = 0;
+    public double calculateFare(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number of rides");
+        int rides = scanner.nextInt();
+        for(int i = 0; i < rides; i++) {
+            System.out.println("Enter the distance travelled");
+            double distance = scanner.nextDouble();
+            System.out.println("Enter the time travelled");
+            double timeInMin = scanner.nextDouble();
+            totalFare = totalFare + (distance * farePerKm) + (timeInMin) + minFare;
+        }
         return totalFare;
     }
 }
--- src/main/java/com/brigelabs_cab_invoice_generator/Main.java
+++ src/main/java/com/brigelabs_cab_invoice_generator/Main.java
@@ -1,17 +1,14 @@
 package com.brigelabs_cab_invoice_generator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class Main {
+    static  double totalFare;
     public static void main(String[] args) {
 
         System.out.println("Welcome to day 30 cab invoice generator program");
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the distance travelled");
-        double distance = scanner.nextDouble();
-        System.out.println("Enter the time travelled");
-        double timeInMin = scanner.nextDouble();
-        double totalFare = invoiceGenerator.calculateFare(distance, timeInMin);
-        System.out.println("Total fare is "+totalFare);
+        System.out.println(invoiceGenerator.calculateFare());
     }
 }
